ba605711583cd297b4a1a9d1bd8b169f8ce41f2b
nilesh507 <nilesh507@gmail.com>
2020-05-23 02:02:31 +0530

    implemented static files

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const app = express();
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 
+
+//use static pages
+app.use(express.static('./assets'))
 //use express-ejs-layouts 
 app.use(expressLayouts);
+// extract styles and scripts from sub pages into the layout 
+app.set("layout extractStyles", true)
+app.set("layout extractScripts", true)
+
+
 // use express router
 app.use('/', require('./routes/index'));
 // set up the view engine

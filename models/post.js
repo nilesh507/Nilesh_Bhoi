6b711392adf1c6bb7cdc9a1ecb058f4479b02d59
nilesh507 <nilesh507@gmail.com>
2020-06-02 13:40:13 +0530

    implemented Schema for comments, added comments to db , finally displayed the comments

--- models/post.js
+++ models/post.js
@@ -9,7 +9,14 @@ const postSchema = new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    //include array of id's of all the comments in this post schema itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 },{
     timestamps: true
 });

b270d597075ca9b743979cf06f63f3dc863db0ad
nilesh507 <nilesh507@gmail.com>
2020-06-03 19:50:10 +0530

    implemented converting to Async Await

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,39 +1,31 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req, res){
-    // return res.end('<h1>Expres is up for codeial!</h1>');
-    // console.log(req.cookies);
-    // res.cookie("user_id", 555);
-    
-    // Post.find({}, function(err, posts){
-    //     return res.render('home.ejs', {
-    //         title : "Home Page",
-    //         posts: posts
-    //     });
-    // });
-    //Populate the user of each post 
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        // find all the user and return them as well 
-        User.find({}, function(err, user){
-            return res.render('home.ejs', {
-                title : "Home Page",
-                posts: posts,
-                all_users: user
-            });
-        });
-        
-    });
-};
+module.exports.home = async function(req, res){
+    try{
+        //Populate the user of each post 
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
 
+        let user = await User.find({});
+
+        return res.render('home.ejs', {
+            title: "Home Page",
+            posts: posts,
+            all_users: user
+        });
+    }catch (err) {
+        console.log('Error ', err);
+        return;
+    }
+    
+}
 
 
 // modulex.exports.actionName = function (req, res){ ....     }

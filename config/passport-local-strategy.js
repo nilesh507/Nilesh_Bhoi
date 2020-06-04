cc8084886f63ca60c3e1f3fb66da4559c2f03422
nilesh507 <nilesh507@gmail.com>
2020-06-04 21:41:59 +0530

    Added flash messages to more actions/controllers

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -4,14 +4,18 @@ const User = require('../models/user');
 
 
 passport.use(new LocalStrategy({
-        usernameField : 'email'
+        usernameField : 'email',
+        passReqToCallback: true,
     },
-    function(email, password, done) {
+    function(req, email, password, done) {
       User.findOne({ email: email }, function (err, user) {
-        if (err) { console.log('Error in findin the user --> Passport'); return done(err); }
+        if (err) { 
+            req.flash('error', err);
+            return done(err); 
+        }
         if (!user || user.password != password) { 
-            console.log('Invalid Username/Password');
-            return done(null, false); 
+            req.flash('error', 'Invalid Username/Password');
+            return done(null, false);  
         }
         return done(null, user);
       });
--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.post('/create',userController.create);
 //Use passport as a middlewareto authenticate
 router.post('/create-session', passport.authenticate(
     'local',
-    {failuerREdirect: '/users/sign-in'}
+    {failureRedirect: '/users/sign-in'}
 
 ), userController.createSession);
 

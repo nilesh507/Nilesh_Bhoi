060e97d498a94c4167530aa4f759c002d00f8d69
nilesh507 <nilesh507@gmail.com>
2020-06-10 21:23:58 +0530

    implemented publishing and deleting comments with ajax and added somme flash messages

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -16,10 +16,24 @@ module.exports.create = async function (req, res) {
             //Comment have been created
             post.comments.push(comment);
             post.save();
+
+            if (req.xhr) {
+                // Similar for comments to fetch the user's id!
+                comment = await comment.populate('user', 'name').execPopulate();
+
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+            req.flash('success', 'Comment published!!');
             res.redirect('/');
         }
     } catch (err) {
-        console.log(err);
+        req.flash('error', err);
         return;
     }
 
@@ -35,14 +49,25 @@ module.exports.destroy = async function (req, res) {
 
             let post = await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
 
+            // send the comment id which was deleted back to the views
+            if (req.xhr) {
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+            req.flash('success', 'Comment deleted!!');
             return res.redirect('back');
 
         } else {
-
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
     } catch (err) {
-        console.log('Error', err);
+        req.flash('error', err);
         return;
     }
 }
--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,82 +1,86 @@
 const User = require("../models/user");
 
-module.exports.profile=function(req,res){
+module.exports.profile = function (req, res) {
     // return res.end('<h1>UserProfile</h1>');
-    User.findById(req.params.id, function(err, user){
+    User.findById(req.params.id, function (err, user) {
         return res.render('user_profile', {
-        title : "User Profile",
-        profile_user: user
+            title: "User Profile",
+            profile_user: user
         });
     });
-    
+
 };
 
 // let's keep it same as before, not changing to Async Await format cause of only one nesting level
-module.exports.update = function(req, res){
-    if (req.user.id = req.params.id ){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+module.exports.update = function (req, res) {
+    if (req.user.id = req.params.id) {
+        User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
+            req.flash('success', 'Updated!');
             return res.redirect('back');
         });
-    }else{
+    } else {
+        req.flash('error', 'Unauthorized!');
         return res.status(401).send('Unauthorized');
     }
 };
 
 
 // Render the sign up page
-module.exports.signUp = function(req, res){
-    if (req.isAuthenticated()){
+module.exports.signUp = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
     return res.render('user_sign_up', {
-        title : "Codeial | Sign Up"
+        title: "Codeial | Sign Up"
     });
 }
 
 // Render the sign in page 
-module.exports.signIn = function(req, res){
-    if (req.isAuthenticated()){
+module.exports.signIn = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
     return res.render('user_sign_in', {
-        title : "Codeial | Sign In"
+        title: "Codeial | Sign In"
     });
 }
 
 
 //get the sign up data 
-module.exports.create = function(req, res){
-    if(req.body.password != req.body.confirm_password){
+module.exports.create = function (req, res) {
+    if (req.body.password != req.body.confirm_password) {
+        req.flash('error', 'Passwords do not match');
         return res.redirect('back');
     }
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){
-            console.log("Error in finding user in signing up" );
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) {
+            req.flash('error', err);
             return;
         }
-        
-        if(!user){  
+
+        if (!user) {
             // console.log(req.body);
-            User.create(req.body, function(err, user){
-                if(err){
-                    console.log("Error in creating user while signing up", err);
+            User.create(req.body, function (err, user) {
+                if (err) {
+                    req.flash('error', err);
                     return;
-                } 
+                }
                 return res.redirect('/users/sign-in');
             });
-        }else{      //password does not match go back to sign up page Ro user already exist 
+        } else {      //password does not match go back to sign up page Ro user already exist 
+            req.flash('success', 'You have signed up, login to continue!');
             return res.redirect('back');
         }
     });
 
 };
 // sign in and create a session for the user
-module.exports.createSession = function(req, res){
+module.exports.createSession = function (req, res) {
     req.flash('success', 'Logged in Successfully');
     return res.redirect('/')
 };
 
-module.exports.destroySession = function(req, res){
+module.exports.destroySession = function (req, res) {
     req.logout();
     req.flash('success', 'You have logged out!');
     return res.redirect('/');

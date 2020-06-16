f9c97425489839e58dff4d4a17b081e56245c1f4
nilesh507 <nilesh507@gmail.com>
2020-06-16 13:29:31 +0530

    implemented rendering an API

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,10 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/v1', require('./v1'))
-
-
-
+router.use('/posts', require('./posts'));
 
 
 module.exports = router;
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
-
-router.use('/v1', require('./v1'))
-
+const postsApi = require('../../../controller/api/v1/posts_api');
 
 
+
+router.get('/', postsApi.index);
 
 
 module.exports = router;

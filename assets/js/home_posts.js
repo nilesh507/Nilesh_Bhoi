af54478c8e1eb82f65877e39076762e57de7c82b
nilesh507 <nilesh507@gmail.com>
2020-06-23 22:46:52 +0530

    corrected comments files of views

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -26,6 +26,7 @@
                         timeout: 1500
 
                     }).show();
+
                 }, error: function (error) {
                     console.log(error.responseText);
                 }
@@ -51,7 +52,7 @@
                     </p>
                     <div class="post-comments">
                         
-                            <form action="/comments/create" method="POST">
+                            <form id="post-${ post._id}-comments-form" action="/comments/create" method="POST">
                                 <input type="text" name="content" placeholder="Type Here to add comment..." required>
                                 <input type="hidden" name="post" value="${ post._id}" >
                                 <input type="submit" value="Add Comment">
@@ -79,6 +80,14 @@
                 url: $(deleteLink).prop('href'),
                 success: function (data) {
                     $(`#post-${data.data.post_id}`).remove();
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post Deleted",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+
+                    }).show();
                 }, error: function (error) {
                     console.log(error.responseText);
                 }
@@ -87,8 +96,11 @@
         });
     }
 
-    // loop over all the existing posts on the page (when the window loads for the first time) and 
-    //call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
+
+
+
+
+    // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
     let convertPostsToAjax = function () {
         $('#posts-list-container>ul>li').each(function () {
             let self = $(this);
@@ -100,8 +112,6 @@
             new PostComments(postId);
         });
     }
-
-
 
 
 
